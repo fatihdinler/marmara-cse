29348d9ea7e67efd74cbff8a47db430aa20b5211
fatiherkamdinler <fatihdinler458@gmail.com>
2022-12-27 19:05:42 +0300

    updated p2

---
+++ CSE 2025 (Data Structures)/project-2/150119567_p2.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct AVLNode {
+    int data;
+    int height;
+    struct AVLNode *leftChild;
+    struct AVLNode *rightChild;
+} AVLNode;
+
+char left_child_rotatin_status[] = "l";
+char right_child_rotatin_status[] = "r";
+int avlRotationNumber = 0;
+
+int getMaxOfNode(int a, int b);
+int getHeigthOfNode(AVLNode *node);
+AVLNode *newAVLNode(int data);
+AVLNode *rotate(AVLNode *rootNode, char status);
+int getBalance(AVLNode *node);
+AVLNode *insert(int data, AVLNode *node);
+void printPreOrderAvlTree(AVLNode *node);
+
+int main() {
+    AVLNode *root = NULL;
+
+    root = insert(44, root);
+    root = insert(3, root);
+    root = insert(12, root);
+    root = insert(10, root);
+    root = insert(6, root);
+    root = insert(3, root);
+    root = insert(25, root);
+    root = insert(21, root);
+    root = insert(38, root);
+    root = insert(12, root);
+    root = insert(123, root);
+    root = insert(43, root);
+    root = insert(45, root);
+    root = insert(56, root);
+    root = insert(67, root);
+    root = insert(78, root);
+    root = insert(89, root);
+    root = insert(90, root);
+    root = insert(12, root);
+    root = insert(0, root);
+    root = insert(348975, root);
+    root = insert(1829734, root);
+    
+
+    printf("Preorder traversal of the constructed AVL tree is: \n");
+    printPreOrderAvlTree(root);
+    printf("\nAVL rotation number is : %dtu\n", avlRotationNumber);
+
+    return 0;
+}
+
+int getHeigthOfNode(AVLNode *node) {
+    if (node == NULL) {
+        return 0;
+    }
+    else {
+        int heightOfNode = node->height;
+        return heightOfNode;
+    }
+}
+
+int getMaxOfNode(int a, int b) {
+    int returnValue = (a > b) ? a : b;
+    return returnValue;
+}
+
+AVLNode *newAVLNode(int data) {
+
+    AVLNode *newlyCreatedNode = (AVLNode *)malloc(sizeof(AVLNode));
+
+    /** The data of the newly created node will be the 'data' which is in function parameter. */
+    newlyCreatedNode->data = data;
+
+    /** The left and rigth child of the newly created node is NULL. */
+    newlyCreatedNode->leftChild = NULL;
+    newlyCreatedNode->rightChild = NULL;
+
+    // We need to set the heigth of the new created node as 1 since every node
+    // that is last created is leaf node. Leaf node's heigth is 1.
+    newlyCreatedNode->height = 1;
+    return newlyCreatedNode;
+}
+
+AVLNode *rotate(AVLNode *rootNode, char status){
+
+    switch (status) {
+
+        case 'r':
+            {
+            // avlRotationNumber++;
+            AVLNode *rootNodeToBeRotated = rootNode->leftChild;
+            AVLNode *tempNode = rootNodeToBeRotated->rightChild;
+
+            rootNodeToBeRotated->rightChild = rootNode;
+            rootNode->leftChild = tempNode;
+
+            int heigthOfRootNodeLeftChild = getHeigthOfNode(rootNode->leftChild);
+            int heigthOfRootNodeRigthChild = getHeigthOfNode(rootNode -> rightChild);
+            int heigthOfRootNode = heigthOfRootNodeLeftChild + heigthOfRootNodeRigthChild + 1;
+            int heigthOfRootNodeToBeRotatedLeftChild = getHeigthOfNode(rootNodeToBeRotated->leftChild);
+            int heigthOfRootNodeToBeRotatedRigthChild = getHeigthOfNode(rootNodeToBeRotated->rightChild);
+            int heigthOfRootNodeToBeRotated = heigthOfRootNodeToBeRotatedLeftChild + heigthOfRootNodeToBeRotatedRigthChild + 1;
+
+            rootNode -> height = heigthOfRootNode;
+            rootNodeToBeRotated->height = heigthOfRootNodeToBeRotated;
+
+            return rootNodeToBeRotated;
+            }
+        case 'l':
+            {
+            // avlRotationNumber++;
+            AVLNode *rootNodeToBeRotated = rootNode->rightChild;
+            AVLNode *tempNode = rootNodeToBeRotated->leftChild;
+
+            rootNodeToBeRotated->leftChild = rootNode;
+            rootNode->rightChild = tempNode;
+
+            int heigthOfRootNodeLeftChild = getHeigthOfNode(rootNode->leftChild);
+            int heigthOfRootNodeRightChild = getHeigthOfNode(rootNode->rightChild);
+            int heigthOfRootNode = heigthOfRootNodeLeftChild + heigthOfRootNodeRightChild + 1;
+            int heigthOfRootNodeToBeRotatedLeftChild = getHeigthOfNode(rootNodeToBeRotated->leftChild);
+            int heigthOfRootNodeToBeRotatedRigthChild = getHeigthOfNode(rootNodeToBeRotated -> rightChild);
+            int heigthOfRootNodeToBeRotated = heigthOfRootNodeToBeRotatedLeftChild + heigthOfRootNodeToBeRotatedRigthChild + 1;
+
+            rootNode -> height = heigthOfRootNode;
+            rootNodeToBeRotated -> height = heigthOfRootNodeToBeRotated;
+
+            return rootNodeToBeRotated;
+            }
+    }
+}
+
+int getBalance(AVLNode *node) {
+
+    if (node == NULL) {
+        return 0;
+    }
+    else {  
+        int leftChildHeigth = getHeigthOfNode(node -> leftChild);
+        int rigthChildHeigth = getHeigthOfNode(node->rightChild);
+        int heigthOfNode = leftChildHeigth - rigthChildHeigth;
+        return heigthOfNode;
+    }
+    
+}
+
+AVLNode *insert(int data, AVLNode *node) {
+
+    /* If the node is initially empty, then create a new node with the given data. */
+    if (node == NULL) {
+        return newAVLNode(data);
+    }
+
+    /** If node exists, then do the basic BST operations. */
+    if(node) {
+        if (data < node->data) {
+            node->leftChild = insert(data , node->leftChild);
+        }
+        else if (data > node->data) {
+            node->rightChild = insert(data , node->rightChild); 
+        }
+        else {
+            return node;
+        }
+        
+        int heigthOfNodeLeftChild = getHeigthOfNode(node->leftChild);
+        int heigthofNodeRigthChild = getHeigthOfNode(node->rightChild);
+        int heigthOfNode = heigthOfNodeLeftChild + heigthofNodeRigthChild + 1;
+        node -> height = heigthOfNode;
+    }
+
+    int balanceFactorOfNode = getBalance(node);
+    
+    if(balanceFactorOfNode) {
+        
+        if (balanceFactorOfNode > 1 && data < node->leftChild->data) {
+            avlRotationNumber++;
+            return rotate(node, 'r');
+        }
+
+        if (balanceFactorOfNode < -1 && data > node->rightChild->data) {
+            avlRotationNumber++;
+            return rotate(node, 'l');
+        }
+
+        if (balanceFactorOfNode > 1 && data > node->leftChild->data) {
+            avlRotationNumber += 2;
+            node->leftChild = rotate(node->leftChild, 'l');
+            return rotate(node, 'r');
+        }
+
+        if (balanceFactorOfNode < -1 && data < node->rightChild->data) {
+            avlRotationNumber += 2;
+            node->rightChild = rotate(node->rightChild, 'r');
+            return rotate(node, 'l');
+        }
+    }
+
+    return node;
+}
+
+void printPreOrderAvlTree(AVLNode *node) {
+    if (node != NULL) {
+        printf("%d ", node->data);
+        printPreOrderAvlTree(node->leftChild);
+        printPreOrderAvlTree(node->rightChild);
+    }
+}
