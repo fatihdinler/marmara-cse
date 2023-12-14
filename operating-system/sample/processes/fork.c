ba3857e67b147736a3fcb550f12b7d7d3190fdaf
fatiherkamdinler <fatihdinler458@gmail.com>
2023-12-14 16:30:56 +0300

    [operating-system] added fork & wait examples

--- operating-system/sample/processes/fork.c
+++ operating-system/sample/processes/fork.c
@@ -1,9 +1,17 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "unistd.h"
 
 int main() {
-  int pid;
+  pid_t pid;
   pid = fork();
 
-  
+  if (pid > 0) {
+    wait(NULL);
+    printf("my pid is %d and my parent pid is %d\n", pid, getpid());
+  } else if (pid == 0) {
+    printf("my pid is %d and my parent pid is %d\n", pid, getpid());
+  } else {
+    printf("Error");
+  }
 }
