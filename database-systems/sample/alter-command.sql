5800aedb096b488b1926d5a6a924904e5b6d273e
fatiherkamdinler <fatihdinler458@gmail.com>
2023-12-18 15:08:13 +0300

    [database-system] added ddl examples

---
+++ database-systems/sample/alter-command.sql
@@ -0,0 +1,13 @@
+use School
+
+-- ! adds a single column to the table
+alter table student 
+add grades INT
+
+-- ! adds multi column to the table
+alter table student
+add entryDate DATETIME, location VARCHAR(40)
+
+-- ! change the data type of a column
+ alter table student 
+ alter column grades SMALLINT
---
+++ database-systems/sample/create-table.sql
@@ -0,0 +1,9 @@
+use School
+
+create table student(
+    number VARCHAR(4),
+    name VARCHAR(12),
+    surname VARCHAR(20),
+    ssn char(11),
+    information text 
+)
