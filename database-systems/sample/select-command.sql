e5285a90e0dd6e26577ac7b55c6392999c19d785
fatiherkamdinler <fatihdinler458@gmail.com>
2024-11-30 15:02:21 +0300

    [database-systems] worked on select commmand & its aliases

---
+++ database-systems/sample/select-command.sql
@@ -0,0 +1,11 @@
+use school;
+
+-- To select all the columns from the Table Student_T, use this command.
+-- SELECT * 
+-- FROM Student_T
+
+-- SELECT name, information
+-- FROM Student_T
+
+-- SELECT S.name, S.ssn
+-- FROM Student_T S
